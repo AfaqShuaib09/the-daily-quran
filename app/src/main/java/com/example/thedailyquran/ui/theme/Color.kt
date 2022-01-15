4ebe783c281d8cecb20803d7bda1e55dd899dbe8
BilalJaved15 <bcsf18m029@pucit.edu.pk>
2022-01-15 22:20:19 +0500

    feat: setup material 3 in jetpack compose

---
+++ app/src/main/java/com/example/thedailyquran/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.thedailyquran.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val bgLight = Color(0xFFFFF6F2)
+val bgDark = Color(0xFF33180C)
+val skin1 = Color(0xFFF89B70)
+val skin2 = Color(0xFFF6987E)
+val orange1 = Color(0xFFF26B55)
+val pink1 = Color(0xFFF25C68)
+val pink2 = Color(0xFFF25C68)
+val green1 = Color(0xFF06BF51)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
