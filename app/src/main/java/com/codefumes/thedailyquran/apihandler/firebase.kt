3d01443b730c4512d138f6af4d5ef206a8b837fe
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2022-01-28 23:01:28 +0500

    feat: supplications module

--- app/src/main/java/com/codefumes/thedailyquran/apihandler/firebase.kt
+++
@@ -1,9 +0,0 @@
-package com.codefumes.thedailyquran.apihandler
-
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-
-fun signInUser() {
-    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestEmail()
-        .build()
-}
---
+++ app/src/main/java/com/codefumes/thedailyquran/data/supplications.kt
@@ -0,0 +1,2 @@
+package com.codefumes.thedailyquran.data
+
